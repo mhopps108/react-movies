ae7d7f8bb1956a480ba910f76711f8faaeb2cbb1
Matt <mhopps108@users.noreply.github.com>
2019-11-15 12:53:58 -0800

    bootstrap work
    working on layout - done for day

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { useDataApi } from "./use-data-api.js";
 
+/*
+MM dd     Title     btn
+Year  Runtime  Cert
+Ratings Imdb tmdb?
+Genres(3)
+*/
 function MovieListItem({ movie }) {
-  //const [id, poster_path, url, title, rating] = props.movie;
   const { id, title, poster_path, release_date, vote_average } = movie;
   const imgUrl = `https://image.tmdb.org/t/p/w92/${poster_path}`;
   const year = release_date.substring(0, 4);
 
+  // try card-deck
   return (
-    //<li>
-    <div className="col-sm-6">
+    <div className="col-sm-6 col-lg-4 px-2 pb-3">
       <div className="card">
+        <div className="card-header p-1 text-center">
+          <p className="card-title m-0">{title}</p>
+        </div>
         <div className="card-body p-1 d-flex">
-          <div className="w-25 img-wrapper">
-            <img src={imgUrl} alt={title} />
+          <div className="flex-shrink-0 h-100">
+            <img className="h-100" src={imgUrl} alt={title} />
           </div>
-          <div className="w-75">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{year}</p>
+          <div className="w-100 p-0 d-flex">
+            <span className="card-text w-25">{year}</span>
+            <span className="card-text w-25">{id}</span>
+            <span className="card-text w-25">{vote_average}</span>
           </div>
         </div>
       </div>
     </div>
-    //</li>
   );
 }
 
@@ -41,13 +49,13 @@ function MovieList() {
   );
 
   return (
-    <>
-      <h1>Now Playing</h1>
+    <div className="movie-list-wrapper mx-auto">
+      <h1 className="text-center">Now Playing</h1>
       <div>
         {isLoading ? (
           <div>Loading ...</div>
         ) : (
-          <div className="movie-list container">
+          <div className="container">
             <div className="row">
               {data.results.map(movie => (
                 <MovieListItem key={movie.id} movie={movie} />
@@ -56,7 +64,7 @@ function MovieList() {
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 }
 

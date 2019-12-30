40a598e447ccfecf37832754978565ef40bfbbd8
Matt <mhopps108@users.noreply.github.com>
2019-12-30 09:36:21 -0800

    backup
    refactor

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,175 +1,22 @@
 import React, { useState, useEffect } from "react";
-import {
-  Card,
-  Calendar,
-  Row,
-  Col,
-  DatePicker,
-  Button,
-  Icon,
-  Drawer
-} from "antd";
-import { discoveryUrlByWeek, buildDiscoveryUrl, movieLists } from "./tmdb-api";
-import { useDataApi } from "./use-data-api.js";
+import { Row, Col } from "antd";
 import MovieListItem from "./MovieListItem";
 import moment from "moment";
-import { Calendar as RMCCalendar } from "rmc-calendar";
-import enUS from "rmc-calendar/lib/locale/en_US";
-import "rmc-calendar/assets/index.css";
-
-const { WeekPicker } = DatePicker;
 
 // NEEDED: data, isLoading, isError
+//function MovieList({ setVisible, releaseType, setListVisible }) {
 
-function MovieList({ setVisible, releaseType, setListVisible }) {
-  const [date, setDate] = useState(moment());
-  const [url, setUrl] = useState(discoveryUrlByWeek());
-  const [{ data, isLoading, isError }, doFetch] = useDataApi(url, {
-    results: []
-  });
-  const [page, setPage] = useState(1);
-
-  const [rmcvisable, setRMCVisable] = useState(false);
-  const [calendarVisible, setCalendarVisible] = useState(false);
-
-  useEffect(() => {
-    setUrl(discoveryUrlByWeek(date, releaseType, page));
-    doFetch(url);
-    console.log("data");
-    console.log(data);
-  }, [date, url, doFetch, releaseType, page]);
-
-  useEffect(() => {
-    console.log("STATE");
-    console.log(`date: ${date}`);
-    console.log(`url: ${url}`);
-  }, [date, url]);
-
-  const dateRangeStr = () => {
-    const s =
-      `${moment(date)
-        .startOf("week")
-        .format("MMM DD YYYY")}` +
-      " to " +
-      `${moment(date)
-        .endOf("week")
-        .format("MMM DD YYYY")}`;
-    return s;
-  };
-
+function MovieList({ movies }) {
+  console.log("MoviesList - Moives:");
+  console.log(movies);
   return (
     <div className="movie-list-wrapper mx-auto">
-      <Row>
-        <Col span={10}>
-          <WeekPicker
-            id={"week-picker"}
-            format={"MMM Do YY"}
-            onChange={date => setDate(moment(date))}
-          />
-        </Col>
-        <Col span={14}>
-          <h4>{dateRangeStr()}</h4>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Button type="primary" onClick={() => setVisible(true)}>
-            Open
-          </Button>
-          <Button type="primary" onClick={() => setRMCVisable(true)}>
-            Cal
-          </Button>
-          <Button type="primary" onClick={() => setListVisible(true)}>
-            List
-          </Button>
-        </Col>
-      </Row>
-      <Row>
-        <Button.Group size="default">
-          <Button onClick={() => setDate(moment(date).subtract(7, "d"))}>
-            <Icon type="left" />
-            Last Week
-          </Button>
-          {/* <Button onClick={() => setCalendarVisible(true)}>
-            <Icon type="calendar" />
-            Calendar
-          </Button> */}
-          <Button onClick={() => setDate(moment())}>
-            <Icon type="calendar" />
-            Today
-          </Button>
-          <Button onClick={() => setDate(moment(date).add(7, "d"))}>
-            Next Week
-            <Icon type="right" />
-          </Button>
-        </Button.Group>
-      </Row>
-      <Row>
-        <Button.Group size="default">
-          <Button
-            disabled={data.page - 1 <= 0}
-            onClick={() => setPage(page => page - 1)}
-          >
-            <Icon type="left" />
-            {data.page - 1}
-          </Button>
-          <Button onClick={() => ""}>
-            {data.page} of {data.total_pages}
-          </Button>
-          <Button onClick={() => ""}>
-            {data.results.length} / {data.total_results}
-          </Button>
-          <Button
-            disabled={data.page + 1 > data.total_pages}
-            onClick={() => setPage(page => page + 1)}
-          >
-            {data.page + 1}
-            <Icon type="right" />
-          </Button>
-        </Button.Group>
-      </Row>
-
-      <div>
-        <RMCCalendar
-          locale={enUS}
-          visible={rmcvisable}
-          onCancel={() => setRMCVisable(false)}
-          onConfirm={""}
-        />
-      </div>
-
-      <Drawer
-        title="Select Week"
-        placement={"bottom"}
-        closable={true}
-        onClose={() => setCalendarVisible(false)}
-        visible={calendarVisible}
-        height={450}
-        zIndex={100}
-      >
-        <div
-          style={{
-            width: "100%",
-            border: "1px solid #d9d9d9",
-            borderRadius: 4
-          }}
-        >
-          <Calendar fullscreen={false} onPanelChange={() => ""} />
-        </div>
-      </Drawer>
-
-      <div>
-        {isLoading ? (
-          <div>Loading ...</div>
-        ) : (
-          <div style={{ background: "#696969", padding: "10px" }}>
-            <Row gutter={[16, 16]}>
-              {data.results.map(movie => (
-                <MovieListItem key={movie.id} movie={movie} />
-              ))}
-            </Row>
-          </div>
-        )}
+      <div style={{ background: "#696969", padding: "10px" }}>
+        <Row gutter={[16, 16]}>
+          {(movies || []).map(movie => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </Row>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { Button, Drawer, Row, Col } from "antd";
+import { Calendar, DatePicker, Icon } from "antd";
+import { discoveryUrlByWeek } from "./tmdb-api";
+import { useDataApi } from "./use-data-api.js";
 import "antd/dist/antd.css";
 import "./styles.css";
 import { MovieList } from "./MovieList";
@@ -9,6 +12,9 @@ import { MovieListDrawer } from "./MovieListDrawer";
 import tmdbData from "./tmdb-data.js";
 import { useTmdbListApi } from "./useTmdbListApi";
 import { Item } from "rc-menu";
+import moment from "moment";
+
+const { WeekPicker } = DatePicker;
 
 //
 
@@ -21,7 +27,49 @@ function App() {
 
   const [listVisible, setListVisible] = useState(false);
   const [{ data, isLoading, isError }, list, setList] = useTmdbListApi();
-  // const tmdbList = tmdbData.tmdbList;
+  const tmdbList = tmdbData.list;
+
+  const [date, setDate] = useState(moment());
+  const [url, setUrl] = useState(discoveryUrlByWeek());
+  // const [{ data, isLoading, isError }, doFetch] = useDataApi(url, {
+  //   results: []
+  // });
+  const [page, setPage] = useState(1);
+
+  // useEffect(() => {
+  //   setUrl(discoveryUrlByWeek(date, releaseType, page));
+  //   doFetch(url);
+  //   console.log("data");
+  //   console.log(data);
+  // }, [date, url, doFetch, releaseType, page]);
+
+  useEffect(() => {
+    console.log(`list: ${list}`);
+    console.log(`data:`);
+    console.log(data);
+    // setList(tmdbList.popular);
+  }, [data]);
+
+  // useEffect(() => {
+  //   // setUrl(discoveryUrlByWeek(date, releaseType, page));
+  //   // doFetch(url);
+  //   // setList()
+  //   console.log("data");
+  //   console.log(data);
+  //   console.log(list);
+  // }, [list]);
+
+  const dateRangeStr = () => {
+    const s =
+      `${moment(date)
+        .startOf("week")
+        .format("MMM DD YYYY")}` +
+      " to " +
+      `${moment(date)
+        .endOf("week")
+        .format("MMM DD YYYY")}`;
+    return s;
+  };
 
   useEffect(() => {
     if (releaseType === 3) {
@@ -38,18 +86,72 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-center">{title}</h1>
-      {data && (
-        <div>
-          {/* <p>{list}</p> */}
-          {/* data.map(item => (<p>{Item.name}</p>
-          )) */}
-        </div>
-      )}
-      <MovieList
-        setVisible={setFilterVisible}
-        setListVisible={setListVisible}
-        releaseType={releaseType}
-      />
+      <div>
+        <Row>
+          <Col span={10}>
+            <WeekPicker
+              id={"week-picker"}
+              format={"MMM Do YY"}
+              onChange={date => setDate(moment(date))}
+            />
+          </Col>
+          <Col span={14}>
+            <h4>{dateRangeStr()}</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button type="primary" onClick={() => setFilterVisible(true)}>
+              Open
+            </Button>
+            <Button type="primary" onClick={() => setListVisible(true)}>
+              List
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Button.Group size="default">
+            <Button onClick={() => setDate(moment(date).subtract(7, "d"))}>
+              <Icon type="left" />
+              Last Week
+            </Button>
+            <Button onClick={() => setDate(moment())}>
+              <Icon type="calendar" />
+              Today
+            </Button>
+            <Button onClick={() => setDate(moment(date).add(7, "d"))}>
+              Next Week
+              <Icon type="right" />
+            </Button>
+          </Button.Group>
+        </Row>
+        <Row>
+          <Button.Group size="default">
+            <Button
+              disabled={data.page - 1 <= 0}
+              onClick={() => setPage(page => page - 1)}
+            >
+              <Icon type="left" />
+              {data.page - 1}
+            </Button>
+            <Button onClick={() => ""}>
+              {data.page} of {data.total_pages}
+            </Button>
+            <Button>
+              {data.length} / {data.total_results}
+            </Button>
+            <Button
+              disabled={data.page + 1 > data.total_pages}
+              onClick={() => setPage(page => page + 1)}
+            >
+              {data.page + 1}
+              <Icon type="right" />
+            </Button>
+          </Button.Group>
+        </Row>
+      </div>
+
+      <MovieList movies={data.results} />
       <MovieListDrawer
         visible={listVisible}
         setVisible={setListVisible}

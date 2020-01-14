4047faad7db9f955c51707830be02c5c680fbeac
Matt <mhopps108@users.noreply.github.com>
2020-01-14 11:17:43 -0800

    bu

--- src/useDataApi.js
+++ src/useDataApi.js
@@ -1,32 +1,91 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
-// TODO: implement cancel with boolean flag
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+};
 
-const useDataApi = (initialUrl, initialData = { results: [] }) => {
-  const [data, setData] = useState(initialData);
+const useDataApi = (initialUrl, initialData) => {
   const [url, setUrl] = useState(initialUrl);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  });
   useEffect(() => {
+    let didCancel = false;
     const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
+      dispatch({ type: 'FETCH_INIT' });
       try {
         const result = await axios(url);
-        console.log(`useDataApi - url\n${url}`);
-        setData(result.data);
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        }
       } catch (error) {
-        setIsError(true);
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_FAILURE' });
+        }
       }
-      setIsLoading(false);
     };
-
     fetchData();
+    return () => {
+      didCancel = true;
+    };
   }, [url]);
+  return [state, setUrl];
 
-  return [{ data, isLoading, isError, setUrl }];
-};
+
+
+// // TODO: implement cancel with boolean flag
+
+// const useDataApi = (initialUrl, initialData = { results: [] }) => {
+//   const [data, setData] = useState(initialData);
+//   const [url, setUrl] = useState(initialUrl);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isError, setIsError] = useState(false);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setIsError(false);
+//       setIsLoading(true);
+//       try {
+//         const result = await axios(url);
+//         console.log(`useDataApi - url\n${url}`);
+//         setData(result.data);
+//       } catch (error) {
+//         setIsError(true);
+//       }
+//       setIsLoading(false);
+//     };
+
+//     fetchData();
+//   }, [url]);
+
+//   return [{ data, isLoading, isError, setUrl }];
+// };
+
+// export { useDataApi };
 
 export { useDataApi };
